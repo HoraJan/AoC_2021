9f701687476906d82d3b08d1b192b20786c29532
Jan Hora <jan.hora@saboit.de>
2021-12-03 08:35:30 +0100

    day 03

--- src/days/index.ts
+++ src/days/index.ts
@@ -1,5 +1,6 @@
 export * as day01 from './01'
 export * as day02 from './02'
+export * as day03 from './03'
 
 export interface Test {
     input: string
--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,30 @@ const red = '\x1b[31m%s\x1b[0m';
 const parts = ['first', 'second'];
 
 try {
-  const [, , regex] = process.argv;
-  if (!regex) throw new Error('you should select single day!');
+    const [, , regex] = process.argv;
+    if (!regex) throw new Error('you should select single day!');
 
-  const day: Day = days[`day${regex}`];
-  if (!day) throw new Error('day not found!');
+    const day: Day = days[`day${regex}`];
+    if (!day) throw new Error('day not found!');
 
-  parts.forEach((part) => {
-    day.tests.forEach((test, index) => {
-      if (!test.results[part]) return;
+    parts.forEach((part) => {
+        day.tests.forEach((test, index) => {
+            if (!test.results[part]) return;
+            const result = day[part](test.input)
 
-      if (test.results[part] === day[part](test.input)) {
-        console.log(green, `✓ test ${index} ${part} part should be ${test.results[part]}`);
-        return;
-      }
+            if (test.results[part] === result) {
+                console.log(green, `✓ test ${index} ${part} part should be ${test.results[part]}`);
+                return;
+            }
 
-      console.log(red, `𐄂 test ${index} ${part} part should be ${test.results[part]} and is ${day[part](test.input)}`);
-      throw new Error(`error in ${part} part`);
+            console.log(red, `𐄂 test ${index} ${part} part should be ${test.results[part]} and is ${result}`);
+            throw new Error(`error in ${part} part`);
+        });
+        console.log('=======');
+        console.log(`result ${part} part is:`);
+        console.log(green, day[part](day.input));
+        console.log('       ');
     });
-    console.log('=======');
-    console.log(`result ${part} part is:`);
-    console.log(green, day[part](day.input));
-    console.log('       ');
-  });
 } catch (err) {
-  console.error(red, err.message);
+    console.error(red, err.message);
 }
