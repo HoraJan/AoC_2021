fbd984a9b5171903e47d52637f85fff540286f8a
Jan Hora <jan.hora@saboit.de>
2021-12-14 09:37:20 +0100

    day 14

--- src/days/14.ts
+++ src/days/14.ts
@@ -1,70 +1,68 @@
 import { Test } from '.';
 
-export const first = (inputString: string) => {
-  const [start, instructionsString] = inputString.split('\n\n')
-  const instructions = instructionsString.split('\n').map(line => line.split(' -> ')).map(([pair, newChar]) => [[pair[0].charCodeAt(0), pair[1].charCodeAt(0)], newChar.charCodeAt(0)])
+const analyseInstructions = (instructionString: string) =>
+  instructionString
+    .split('\n')
+    .map(line => line.split(' -> '))
+    .reduce((acc, [pair, newOne]) => {
+      acc[pair] = newOne
+      return acc
+    }, {})
 
-  let product = start.split('').map(char => char.charCodeAt(0))
 
-  for (let step = 0; step < 10; step++) {
-    const newProduct = []
-    product.forEach((char, index) => {
-      if (index === 0) {
-        newProduct.push(char)
+const prepareStep = (start: string) => start.split('').reduce((acc, char, index) => {
+  if (index === 0) return acc
 
-        return
-      }
-      const instruction = instructions.find(inst => inst[0][0] === product[index - 1] && inst[0][1] === char)
+  if (!acc[start[index - 1] + char]) acc[start[index - 1] + char] = 0
 
-      newProduct.push(instruction[1])
-      newProduct.push(char)
-    })
+  acc[start[index - 1] + char] += 1
 
-    product = newProduct
-    // console.log(product)
-  }
+  return acc
+}, {})
+
+const solveStep = (product: { [key: string]: number }, instructions: { [key: string]: string }) =>
+  Object.entries(product).reduce((acc, [pair, count]) => {
+    const instruction = instructions[pair]
+    if (!acc[pair[0] + instruction]) acc[pair[0] + instruction] = 0
+    if (!acc[instruction + pair[1]]) acc[instruction + pair[1]] = 0
+
+    acc[pair[0] + instruction] += count
+    acc[instruction + pair[1]] += count
+
+    return acc
+  }, {})
+
+
+const calculateDiff = (product: { [key: string]: number }, start: string) => {
   const sums = {}
-  product.forEach(char => {
-    if (!sums[char]) sums[char] = 0
-    sums[char] += 1
-  })
-  const sorted = Object.values(sums).sort((a: number, b: number) => a - b)
+  // we use first letter or start string
+  sums[start[0]] = 1
 
-  return - +sorted[0] + +sorted[sorted.length - 1]
+  // and second letter of each pair
+  Object.entries(product).forEach(([[, secondChar], count]) => {
+    if (!sums[secondChar]) sums[secondChar] = 0
+    sums[secondChar] += count
+  })
+  const sorted = Object.values<number>(sums).sort((a: number, b: number) => a - b)
+
+  return sorted[sorted.length - 1] - sorted[0]
 }
 
-export const second = (inputString: string) => {
+const solve = (inputString: string, steps: number) => {
   const [start, instructionsString] = inputString.split('\n\n')
-  const instructions = instructionsString.split('\n').map(line => line.split(' -> ')).map(([pair, newChar]) => [[pair[0].charCodeAt(0), pair[1].charCodeAt(0)], newChar.charCodeAt(0)])
+  const instructions = analyseInstructions(instructionsString)
+  let product = prepareStep(start)
 
-  let product = start.split('').map(char => char.charCodeAt(0))
-
-  for (let step = 0; step < 40; step++) {
-    console.log(step)
-    const newProduct = []
-    product.forEach((char, index) => {
-      if (index === 0) {
-        newProduct.push(char)
-
-        return
-      }
-      const instruction = instructions.find(inst => inst[0][0] === product[index - 1] && inst[0][1] === char)
-
-      newProduct.push(instruction[1])
-      newProduct.push(char)
-    })
-
-    product = newProduct
+  for (let step = 0; step < steps; step++) {
+    product = solveStep(product, instructions)
   }
-  const sums = {}
-  product.forEach(char => {
-    if (!sums[char]) sums[char] = 0
-    sums[char] += 1
-  })
-  const sorted = Object.values(sums).sort((a: number, b: number) => a - b)
 
-  return - +sorted[0] + +sorted[sorted.length - 1]
+  return calculateDiff(product, start)
 }
+
+export const first = (inputString: string) => solve(inputString, 10)
+
+export const second = (inputString: string) => solve(inputString, 40)
 
 export const tests: Test[] = [
   {
